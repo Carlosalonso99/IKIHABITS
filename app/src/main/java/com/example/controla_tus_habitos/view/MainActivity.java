dc5ebec2c6d5f26b0a6830ccb7642c9fc1bbec4b
Carlos <carlosalonsomingo@gmail.com>
2024-02-02 15:08:52 +0100

    Menu base implementado

--- app/src/main/java/com/example/controla_tus_habitos/view/MainActivity.java
+++ app/src/main/java/com/example/controla_tus_habitos/view/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.controla_tus_habitos.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.controla_tus_habitos.R;
@@ -36,9 +40,33 @@ public class MainActivity extends AppCompatActivity {
             });
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Infla el menú; esto añade ítems a la barra de acción si está presente.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.itemHome) {
+            // Maneja el clic en el ítem de configuración aquí
+            Toast.makeText(this, "Hola", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         habitoRep = HabitoRepository.getInstance(this);
 
         recyclerView = findViewById(R.id.rvHabitos);
@@ -56,6 +84,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
 
     /**
      * Recargar la lista de hábitos desde la base de datos
